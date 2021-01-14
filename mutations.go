237d3d8f26a5d3611d45c8ddd94bd03779e4267e
Daniel <anonymous@example.com>
2021-01-14 09:37:53 +0000

    Added Amit Klein's "challenges" to the mutations

--- mutations.go
+++ mutations.go
@@ -3,11 +3,44 @@ package main
 func generateMutations() map[string]string {
 	m := make(map[string]string, 0)
 
-	m["id"] = "Content-Length: %s"
+	m["identity"] = "Content-Length: %s"
+
 	m["colon-prefix-chars"] = "Content-Length abcd: %s"
 	m["colon-prefix-space"] = "Content-Length : %s"
+	m["colon-prefix-null"] = "Content-Length\x00: %s"
+	m["colon-prefix-tab"] = "Content-Length\t: %s"
+	m["colon-prefix-vtab"] = "Content-Length\x0b: %s"
+	m["colon-prefix-nl"] = "Content-Length\n: %s"
+	m["colon-prefix-cr"] = "Content-Length\r: %s"
 
-	m["fake"] = "Not-A-Thing: %s"
+	m["colon-post-chars"] = "Content-Length:abcd %s"
+	m["colon-post-null"] = "Content-Length:\x00%s"
+	m["colon-post-tab"] = "Content-Length:\t%s"
+	m["colon-post-vtab"] = "Content-Length:\x0b%s"
+	m["colon-post-nl"] = "Content-Length:\n%s"
+	m["colon-post-cr"] = "Content-Length:\r%s"
+
+	m["line-prefix-space"] = " Content-Length: %s"
+	m["line-prefix-tab"] = "\tContent-Length: %s"
+	m["line-prefix-vtab"] = "\x0bContent-Length: %s"
+	m["line-prefix-null"] = "\x00Content-Length: %s"
+	m["line-prefix-cr"] = "\rContent-Length: %s"
+	m["line-prefix-nl"] = "\nContent-Length: %s"
+
+	m["nl"] = "X-Header: y\nContent-Length: %s"
+	m["cr"] = "X-Header: y\rContent-Length: %s"
+	m["double-nl"] = "X-Header: y\n\nContent-Length: %s"
+	m["double-cr"] = "X-Header: y\r\rContent-Length: %s"
+
+	m["nospace"] = "Content-Length:%s"
+	m["single-quotes"] = "Content-Length: '%s'"
+	m["double-quotes"] = "Content-Length: \"%s\""
+	m["uppercase"] = "CONTENT-LENGTH: %s"
+	m["hex"] = "Content-Length: 0x%s"
+
+	// Amit Klein's "challenges"
+	m["cr-hyphenated"] = "Content\rLength: %s"
+	m["signed"] = "Content-Length: +%s"
 
 	return m
 }
